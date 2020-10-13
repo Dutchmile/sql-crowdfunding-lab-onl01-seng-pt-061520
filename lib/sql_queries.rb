e1ee9819f111b107cf835e839960faff9c8b0ecf
Christopher Dutcher <cadutcher@icloud.com>
2020-10-13 15:10:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,11 +27,11 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "ALTER TABLE projects ADD COLUMN amount_over INTEGER
-  
-  SELECT projects.title, 
-  (SUM(pledges.amount) - projects.funding_goal) FROM projects
-  INNER JOIN pledges 
-  ON projects.id = pledges.project_id
+  UPDATE projects
+  SET    amount_over =  (SELECT   SUM(pledges.amount)
+                          FROM    pledges
+                          GROUP BY pledges.project_id) - projects.funding_goal
+  SELECT projects.title, projects.amount_over FROM projects
   WHERE (SUM(pledges.amount) - projects.funding_goal) > 0
   GROUP BY projects.title;"
 end
